c4912ca54f062fd34b66b37150f235a7ee084c64
PSP VU21CSEN0101025 <93901037+jishnu1515@users.noreply.github.com>
2021-11-25 18:21:02 +0530

    Create odd for.c

---
+++ odd for.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    printf("to find the odd numbers of 1 to 50");
+    for(i=1;i<50;i+=2)
+    {
+        printf("%d",i);
+    }
+
+    return 0;
+}
