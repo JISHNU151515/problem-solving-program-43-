95ae5cbfa60a2b45c24eee68e2560ffab2cb41c5
JISHNU151515 <93901037+JISHNU151515@users.noreply.github.com>
2021-11-25 15:35:33 +0530

    Create circumference circle.c

---
+++ circumference circle.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float r,c,d,a,l,m;
+    printf("to find the circumference and area of the circle");
+    scanf("%f",&a);
+    scanf("%f",&l);
+    scanf("%f",&m);
+    a=180*r*r;
+    l=2*r;
+    m=c/(2*180);
+    printf("%f %f %f",a,l,m);
+    return 0;
+}
