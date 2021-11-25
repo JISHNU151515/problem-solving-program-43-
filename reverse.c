8ca6df1ba53189ed40c2f7256b97c8c8bb7a4975
PSP VU21CSEN0101025 <93901037+jishnu1515@users.noreply.github.com>
2021-11-25 18:14:49 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a=10;
+    printf("to find the how many members in the class");
+    for(a=10;a>=0;a--)
+    {
+        printf("%d \t",a);
+    }
+
+    return 0;
+}
