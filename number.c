0174f07ed47f3d0caefcd90bce03b87f8ab29c50
PSP VU21CSEN0101025 <93901037+jishnu1515@users.noreply.github.com>
2021-11-25 18:18:35 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("by printing the numbers");
+    scanf("%d",&n);
+    switch(n)
+    {
+        case 1:
+        printf("one");
+        break;
+        case 2:
+        printf("two");
+        break;
+        case 3:
+        printf("three");
+        break;
+        case 4:
+        printf("four");
+        break;
+        case 5:
+        printf("five");
+        break;
+        default:
+        printf("please enter the number properly");
+    }
+
+    return 0;
+}
