eb27b602420fd0f395787472b876862b8a4ebcb7
PSP VU21CSEN0101025 <93901037+jishnu1515@users.noreply.github.com>
2021-11-25 18:07:42 +0530

    Create arthimatic all.c

---
+++ arthimatic all.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d,e;
+    float g;
+    printf("to perform all arthematics in same variables");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    g=a/b;
+    printf("%d %d %d \n",c,d,e);
+    printf("%f \n",g);
+    return 0;
+}
