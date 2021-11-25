541b13ae3f5621a035409e88eebf61cd993699ba
PSP VU21CSEN0101025 <93901037+jishnu1515@users.noreply.github.com>
2021-11-25 18:32:13 +0530

    Create voting.c

---
+++ voting.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    printf("to find the member for voting eligiable are not");
+    scanf("%d",&x);
+    if(x>18)
+    {
+        printf("you can eligiable for voting");
+    }
+    else
+    {
+        printf("you are not eligiable for voting");
+    }
+
+    return 0;
+}
