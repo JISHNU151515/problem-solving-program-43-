94d0525da63dd8d5127f393757111bd7a34556be
PSP VU21CSEN0101025 <93901037+jishnu1515@users.noreply.github.com>
2021-11-25 18:13:16 +0530

    Create number loop.c

---
+++ number loop.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a=20,x;
+    printf("to increase the value of a");
+    x=a++;
+    printf("%d",a++);
+    
+
+
+    return 0;
+}
