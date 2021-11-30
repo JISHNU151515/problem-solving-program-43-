ab0ea301d36592c1306cace5975867e9e87802e8
PSP VU21CSEN0101025 <93901037+jishnu1515@users.noreply.github.com>
2021-11-30 10:45:44 +0530

    Create switch vowel.c

---
+++ switch vowel.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int main()
+{
+    char a,e,i,o,u,n;
+    scanf("%c",&n);
+    switch(n)
+    {
+        case 'a':printf("it is an vowel");
+        break;
+        case 'e':printf("it is an vowel");
+        break;
+        case 'i':printf("it is an vowel");
+        break;
+        case 'o':printf("it is an vowel");
+        break;
+        case 'u':printf("it is an vowel");
+        break;
+        default:
+        printf("it is not an vowel");
+    }
+
+    return 0;
+}
