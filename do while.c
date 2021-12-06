c7c77b181e91c386bf7dd9ac40cd7cca1a564cf5
PSP VU21CSEN0101025 <93901037+jishnu1515@users.noreply.github.com>
2021-12-06 12:14:40 +0530

    Create do while.c

---
+++ do while.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i=1,n;
+    printf("enter the number");
+    scanf("%d",&n);
+    do
+    {
+        printf("jishnu \n");
+        i++;
+    }
+    while(i<=n);
+
+    return 0;
+}
