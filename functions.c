80ceaaf7dcc3f61118907b628c77c5bd48e3e3c9
PSP VU21CSEN0101025 <93901037+jishnu1515@users.noreply.github.com>
2022-01-31 11:46:17 +0530

    Create functions.c

---
+++ functions.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+void add();
+void sub();
+void mul();
+void div();
+{
+    add();
+    sub();
+    mul();
+    div();
+    
+}
+void add()
+{
+    scanf
+    c=a+b;
+    printf("%d",c);
+}
+
+
+    return 0;
+}
+
