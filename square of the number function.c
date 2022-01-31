773c7d8642eebd18348fac529abfa40f81d0de2c
PSP VU21CSEN0101025 <93901037+jishnu1515@users.noreply.github.com>
2022-01-31 11:48:30 +0530

    Create square of the number function.c

---
+++ square of the number function.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a,x;
+    scanf("%d",&x);
+    a=square(x);
+    printf("%d",a);
+    return 0;
+    
+}
+int square(int x)
+{
+    a=x*x;
+    return(a);
+}
+
