fe6ae915f5df7f7e099a72683f4f2c01ac2e0367
PSP VU21CSEN0101025 <93901037+jishnu1515@users.noreply.github.com>
2022-01-31 11:43:08 +0530

    Create reverse of the numbers.c

---
+++ reverse of the numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int x[3],i;
+    printf("to print the reverse numbers");
+    for(i=3;i>0;i--)
+    {
+        printf("%d",x[i]);
+        printf("\n");
+    }
+    return 0;
+    
+}
